f3de0a3b0fb96d921e00678280764eb08e1f04fc
JM <goten60@gmail.com>
2025-02-24 18:01:40 -0300

    cambio script dotnet6

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -12,14 +12,14 @@ sudo yum update -y
 echo "sudo yum install -y libunwind libicu"
 sudo yum install -y libunwind libicu
 
-echo "sudo rpm"
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo "sudo sh"
-sudo sh -c 'echo -e "[packages-microsoft-com-prod]\nname=packages-microsoft-com-prod \nbaseurl=https://packages.microsoft.com/yumrepos/microsoft-rhel8.3-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/dotnetdev.repo'
+#echo "sudo rpm"
+#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+#echo "sudo sh"
+#sudo sh -c 'echo -e "[packages-microsoft-com-prod]\nname=packages-microsoft-com-prod \nbaseurl=https://packages.microsoft.com/yumrepos/microsoft-rhel8.3-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/dotnetdev.repo'
 
 # Install the .NET 8.0 runtime
-echo "sudo yum install -y dotnet-sdk-8.0"
-sudo yum install -y dotnet-sdk-8.0
+#echo "sudo yum install -y dotnet-sdk-8.0"
+#sudo yum install -y dotnet-sdk-8.0
 
 # Verify the installation
 echo "dotnet --"
